24af15526648cff3f3da14d1ddde44a6161986ff
Daniel César <144414027+Dasksz@users.noreply.github.com>
2026-05-19 14:52:26 -0300

    Add files via upload

---
+++ AUTOMACAO_ENVIO_DE_DOCUMENTOS/Gerar_Lote.bat
@@ -0,0 +1,11 @@
+@echo off
+title Preparar Lote de Holerites - RH Prime
+color 0B
+echo ===================================================
+echo   SISTEMA DE RH - LER PDF E CRIAR LOTE DE ENVIO
+echo ===================================================
+echo.
+python 1_Preparar_Holerites.py
+echo.
+echo ===================================================
+pause
